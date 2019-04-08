f168d8fc4ab96e80d0a907f76419622a7d470560
HenryITS <37750623+HenryITS@users.noreply.github.com>
2019-04-08 21:03:36 +0200

    text of help modified

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'This is some helpful text.',
+        helpText: 'If you need help, send me an email.',
         title: 'Help',
         name: 'Henry IT Solutions'
     })
