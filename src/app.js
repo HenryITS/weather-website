45759c35311c9fb6d1e62f30a1ad0971571dc1b1
HenryITS <37750623+HenryITS@users.noreply.github.com>
2019-04-09 13:55:35 +0200

    message in console app.js changed

--- src/app.js
+++ src/app.js
@@ -104,6 +104,6 @@ app.get('*', (req, res) => {
 // Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+  console.log(`App listening and running on port ${PORT}`);
   console.log('Press Ctrl+C to quit.');
 });
